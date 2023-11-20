9a421de59ddeb0666e19747afb78e9cbe7d12c16
sphirye <ronielcontreras70@gmail.com>
2023-11-20 11:48:25 -0500

    Add AdBellumLogo.vue in header

---
+++ src/components/AdBellumLogo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div
+        class="text-20 ml-1 blackmoor-text"
+        style="line-height: 20px !important;"
+    >
+      Ad
+    </div>
+    <div
+        class="text-30 blackmoor-text"
+        style="line-height: 25px !important;"
+    >
+      Bellum
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+
+@Component({})
+export default class AdBellumLogo extends Vue {
+
+}
+</script>
--- src/components/ui/HeaderComponent.vue
+++ src/components/ui/HeaderComponent.vue
@@ -1,15 +1,18 @@
 <template>
 	<v-app-bar color="grey darken-4" dark app clipped-left flat>
     <v-app-bar-nav-icon @click.stop="drawer = !drawer"/>
-		<div class="d-flex align-center pointer" @click="$router.push('/').catch(err => {})">
-			<v-img alt="Vuetify Logo" class="shrink mr-2" contain src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" transition="scale-transition" width="40"/>
-			<v-img alt="Vuetify Name" class="shrink mt-1 hidden-sm-and-down" contain min-width="100" src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png" width="100"/>
-		</div>
+
+		<div class="pointer ml-2">
+      <AdBellumLogo @click="$router.push('/').catch(err => {})"/>
+    </div>
+
 		<v-spacer/>
+
 		<v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
 			<span class="mr-2">Latest Release</span>
 			<v-icon>mdi-open-in-new</v-icon>
 		</v-btn>
+
 	</v-app-bar>
 </template>
 
@@ -18,8 +21,11 @@ import { Component, Vue } from "vue-property-decorator"
 import {getModule} from "vuex-module-decorators"
 import LangModule from "@/store/LangModule"
 import DrawerModule from "@/store/DrawerModule";
+import AdBellumLogo from "@/components/AdBellumLogo.vue";
 
-@Component
+@Component({
+  components: {AdBellumLogo}
+})
 export default class HeaderComponent extends Vue {
 	lang = getModule(LangModule).lang
   drawerModule:DrawerModule = getModule(DrawerModule)
