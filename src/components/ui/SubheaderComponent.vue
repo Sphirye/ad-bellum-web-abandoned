7f0e5ccefa9ad927d3528d550bb5355ecc022e1e
sphirye <ronielcontreras70@gmail.com>
2023-11-20 19:16:50 -0500

    Add SubheaderComponent.vue

---
+++ src/components/ui/SubheaderComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row align="center" justify="center">
+    <v-tabs centered>
+      <v-tab>Inicio</v-tab>
+      <v-tab>Combates</v-tab>
+      <v-tab>Eventos</v-tab>
+      <v-tab>Galería</v-tab>
+      <v-tab>Acerca de</v-tab>
+    </v-tabs>
+  </v-row>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+
+@Component
+export default class SubheaderComponent extends Vue {
+
+}
+</script>
