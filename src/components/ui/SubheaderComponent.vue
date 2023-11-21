e2bb8bd7ac9c8fe8739e08460f55255224b86634
sphirye <ronielcontreras70@gmail.com>
2023-11-21 10:06:34 -0500

    Define tab routes by iterating them from an array in SubheaderComponent.vue

--- src/components/ui/SubheaderComponent.vue
+++ src/components/ui/SubheaderComponent.vue
@@ -1,11 +1,9 @@
 <template>
   <v-row align="center" justify="center">
     <v-tabs centered>
-      <v-tab>Inicio</v-tab>
-      <v-tab>Combates</v-tab>
-      <v-tab>Eventos</v-tab>
-      <v-tab>Galería</v-tab>
-      <v-tab>Acerca de</v-tab>
+      <template v-for="(tab, key) in tabs">
+        <v-tab link :to="tab.route" :key="key">{{ tab.name }}</v-tab>
+      </template>
     </v-tabs>
   </v-row>
 </template>
@@ -15,6 +13,14 @@ import {Vue, Component} from 'vue-property-decorator';
 
 @Component
 export default class SubheaderComponent extends Vue {
+
+  tabs = [
+    { name: "Inicio", route: "/home" },
+    { name: "Combates", route: "/combats" },
+    { name: "Eventos", route: "/events" },
+    { name: "Galería", route: "/galery" },
+    { name: "Acerca de", route: "/about" },
+  ]
 
 }
 </script>
