6bf5be8f845f63dfe4bed9fb91e99fa539e9fb2a
sphirye <ronielcontreras70@gmail.com>
2023-11-21 09:57:51 -0500

    Add new subheader views

---
+++ src/views/EventsView.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container fluid>
+
+  </v-container>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+
+@Component
+export default class EventsView extends Vue {
+
+}
+</script>
